5c2b6942f46b4e961eb29e9cf69f6ce03aa72e1a
Alexander <sanya_666@mail.ru>
2021-03-03 12:15:13 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace island_area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
