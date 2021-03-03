5b7eba918c8bbba11c2653942e6e3c816d7767d9
Alexander <sanya_666@mail.ru>
2021-03-03 12:53:14 +0600

    Added a map generation and printing.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,36 @@ namespace island_area
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] map = GenerateMap();
+            ShowMap(map, false);
+            ShowMap(map, true);
+        }
+
+        public static void ShowMap(int[,] map, bool show_land_mass = false)
+        {
+            for (int h = 0; h < map.GetUpperBound(1); h++)
+            {
+                for (int w = 0; w < map.GetUpperBound(0); w++)
+                    if (show_land_mass == false)
+                        Console.Write(map[w, h]);
+                    else if (map[w, h] > 0)
+                        Console.Write("#");
+                    else
+                        Console.Write(" ");
+                Console.WriteLine();
+            }
+        }
+
+        public static int[,] GenerateMap(int width = 20, int height = 7)
+        {
+            int[,] map = new int[width, height];
+            Random rnd = new Random();
+
+            for (int w = 0; w < map.GetUpperBound(0); w++)
+                for (int h = 0; h < map.GetUpperBound(1); h++)
+                    map[w, h] = rnd.Next(2);
+
+            return map;
         }
     }
 }
